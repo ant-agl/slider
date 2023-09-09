b21d203ebaceced81da0ff5366ac1f0bb543f657
ant-agl <aglanton31@gmail.com>
2023-09-10 01:58:00 +0300

    up

--- js/levels-slider.js
+++ js/levels-slider.js
@@ -1,117 +1,129 @@
 let levelsData = [];
 (async () => {
   levelsData = await getLevelsData();
-})()
+})();
 function getLevelsData() {
-  return new Promise(resolve => {
-    $.getJSON('js/levels-data.json', data => {
-    resolve(data);
-  });
+  return new Promise((resolve) => {
+    $.getJSON("js/levels-data.json", (data) => {
+      resolve(data);
+    });
   });
 }
 
 function prevLevel() {
-  let itemIndex = $('.levels-slider__content > .active').index();
+  let itemIndex = $(".levels-slider__content > .active").index();
   if (itemIndex == 0) return;
 
   toLevel(itemIndex - 1);
 }
 function nextLevel() {
-  let itemIndex = $('.levels-slider__content > .active').index();
-  if (itemIndex == levelsData.length-1) return;
+  let itemIndex = $(".levels-slider__content > .active").index();
+  if (itemIndex == levelsData.length - 1) return;
 
   toLevel(itemIndex + 1);
 }
-function toLevel(itemIndex, data=false) {
+function toLevel(itemIndex, data = false) {
   if (data) levelsData = data;
 
-  $('.levels-slider__content > *').removeClass('active');
-  $('.levels-slider__content > *').eq(itemIndex).addClass('active');
+  $(".levels-slider__content > *").removeClass("active");
+  $(".levels-slider__content > *").eq(itemIndex).addClass("active");
 
   let slide = parseInt(itemIndex / ITEMS_IN_SLIDE);
-  $('.levels-slider__dot').removeClass('active');
-  $('.levels-slider__dot').eq(slide).addClass('active');
+  $(".levels-slider__dot").removeClass("active");
+  $(".levels-slider__dot").eq(slide).addClass("active");
 
-  $('[data-slider-arrow]').removeClass('disable');
+  $("[data-slider-arrow]").removeClass("disable");
   if (itemIndex == 0) {
-    $('[data-slider-arrow="prev"]').addClass('disable');
+    $('[data-slider-arrow="prev"]').addClass("disable");
   }
-  if (itemIndex == levelsData.length-1) {
-    $('[data-slider-arrow="next"]').addClass('disable');
+  if (itemIndex == levelsData.length - 1) {
+    $('[data-slider-arrow="next"]').addClass("disable");
   }
   if (slide == 0) {
-    $('[data-slider-arrow="prev-page"]').addClass('disable');
+    $('[data-slider-arrow="prev-page"]').addClass("disable");
   }
-  if (slide == parseInt(levelsData.length / ITEMS_IN_SLIDE)-1) {
-    $('[data-slider-arrow="next-page"]').addClass('disable');
+  if (slide == parseInt(levelsData.length / ITEMS_IN_SLIDE) - 1) {
+    $('[data-slider-arrow="next-page"]').addClass("disable");
   }
 
-  let wContent = $('.levels-slider__content').width();
+  let wContent = $(".levels-slider__content").width();
   if ($(window).outerWidth() > 767) {
-    if ($('.levels-slider__content').scrollLeft() != wContent * slide)
-      $('.levels-slider__content').animate({scrollLeft: wContent * slide}, 400, 'linear');
-   } else {
-    let wBlock = $('.level-block').outerWidth();
-    $('.levels-slider__content').animate({scrollLeft: (wBlock * itemIndex) - (wContent / 2) + (wBlock / 2)}, 200, 'linear');
-   }
-  
+    if ($(".levels-slider__content").scrollLeft() != wContent * slide)
+      $(".levels-slider__content").animate(
+        { scrollLeft: wContent * slide },
+        400,
+        "linear"
+      );
+  } else {
+    let wBlock = $(".level-block").outerWidth();
+    $(".levels-slider__content").animate(
+      { scrollLeft: wBlock * itemIndex - wContent / 2 + wBlock / 2 },
+      200,
+      "linear"
+    );
+  }
+
   updateData(itemIndex, data);
 }
-function updateData(itemIndex, data=false) {
+function updateData(itemIndex, data = false) {
   if (data) levelsData = data;
 
   let level = levelsData[itemIndex];
-  
+
   // main img
-  $('.loyality-program__main-img .image').remove();
-  $('.loyality-program__main-img').append(`
-    <img src="/img/${level.img}" class="image">
+  $(".loyality-program__main-img .image").remove();
+  $(".loyality-program__main-img").append(`
+    <img src="img/${level.img}" class="image">
   `);
 
   // level
-  $('.loyality-program__level').text('Level ' + level.level);
+  $(".loyality-program__level").text("Level " + level.level);
 
   // info
-  $('.level-reward').text(level.reward);
-  $('.level-type').text(level.type);
-  $('.level-wager').text(level.wager);
-  $('.level-more').text(level.more);
-  $('.more-info__text').text(level.moreInfo);
+  $(".level-reward").text(level.reward);
+  $(".level-type").text(level.type);
+  $(".level-wager").text(level.wager);
+  $(".level-more").text(level.more);
+  $(".more-info__text").text(level.moreInfo);
   let textBtn = `Level ${level.level} more info`;
-  $('.btn-level-more-info').data('text', textBtn).text(textBtn);
+  $(".btn-level-more-info").data("text", textBtn).text(textBtn);
 
+  $(".level-type").closest("li").show();
+  if (level.type == "") $(".level-type").closest("li").hide();
 
-  $('.level-type').closest('li').show();
-  if (level.type == '') $('.level-type').closest('li').hide();
+  $(".level-wager").closest("li").show();
+  if (level.wager == "") $(".level-wager").closest("li").hide();
 
-  $('.level-wager').closest('li').show();
-  if (level.wager == '') $('.level-wager').closest('li').hide();
+  $(".level-more").closest(".loyality-program__more-block").show();
+  if (level.more == "")
+    $(".level-more").closest(".loyality-program__more-block").hide();
 
-  $('.level-more').closest('.loyality-program__more-block').show();
-  if (level.more == '') $('.level-more').closest('.loyality-program__more-block').hide();
-
-  $('.more-info').show();
-  if (level.moreInfo == '') {
-    $('.more-info').hide();
+  $(".more-info").show();
+  if (level.moreInfo == "") {
+    $(".more-info").hide();
   }
 
   // animate
-  $('.loyality-program__main-img .image').css('opacity', 0).animate({
-    opacity: 1,
-  }, 'fast', 'linear');
-  $('.loyality-program__info-block')
+  $(".loyality-program__main-img .image").css("opacity", 0).animate(
+    {
+      opacity: 1,
+    },
+    "fast",
+    "linear"
+  );
+  $(".loyality-program__info-block")
     .css({
-      transition: '0s',
-      opacity: '0',
-      transform: 'translateX(-70px)'
+      transition: "0s",
+      opacity: "0",
+      transform: "translateX(-70px)",
     })
     .delay(10)
     .queue((next) => {
-      $('.loyality-program__info-block').css({
-        transition: '0.7s',
-        opacity: '1',
-        transform: 'translateX(0)'
-      })
+      $(".loyality-program__info-block").css({
+        transition: "0.7s",
+        opacity: "1",
+        transform: "translateX(0)",
+      });
       next();
     });
 
@@ -120,42 +132,42 @@ function updateData(itemIndex, data=false) {
   // $('.loyality-program__main-img').outerHeight(h);
 }
 
-$('.levels-slider__content').on('click', function(e) {
-  let $item = $(e.target).closest('.level-block');
-  if ($item.hasClass('active')) return;
+$(".levels-slider__content").on("click", function (e) {
+  let $item = $(e.target).closest(".level-block");
+  if ($item.hasClass("active")) return;
 
-  toLevel($item.data('index'));
+  toLevel($item.data("index"));
 });
-$('[data-slider-arrow]').on('click', function() {
-  let direction = $(this).data('sliderArrow');
+$("[data-slider-arrow]").on("click", function () {
+  let direction = $(this).data("sliderArrow");
   switch (direction) {
-    case 'prev':
+    case "prev":
       prevLevel();
       break;
-    case 'next':
+    case "next":
       nextLevel();
       break;
-    case 'prev-page':
-      $('.levels-slider__dot.active').prev().trigger('click');
+    case "prev-page":
+      $(".levels-slider__dot.active").prev().trigger("click");
       break;
-    case 'next-page':
-      $('.levels-slider__dot.active').next().trigger('click');
+    case "next-page":
+      $(".levels-slider__dot.active").next().trigger("click");
       break;
-    case 'start':
+    case "start":
       toLevel(0);
       break;
-    case 'end':
-      toLevel(levelsData.length-1);
+    case "end":
+      toLevel(levelsData.length - 1);
       break;
     default:
-      let lvl = $(this).data('sliderArrow');
+      let lvl = $(this).data("sliderArrow");
       toLevel(lvl);
       break;
   }
 });
-$('.levels-slider__dots').on('click', function(e) {
-  let $item = $(e.target).closest('.levels-slider__dot');
-  if ($item.hasClass('active')) return;
+$(".levels-slider__dots").on("click", function (e) {
+  let $item = $(e.target).closest(".levels-slider__dot");
+  if ($item.hasClass("active")) return;
 
-  toLevel($item.data('index') * ITEMS_IN_SLIDE);
+  toLevel($item.data("index") * ITEMS_IN_SLIDE);
 });
--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 // more-info
-$('.more-info').each((i, el) => {
+$(".more-info").each((i, el) => {
   let text = $(el).text();
   $(el).html(`
-    <img src="/img/question.png" class="more-info__img">
+    <img src="img/question.png" class="more-info__img">
     <span class="more-info__text">${text}</span>
   `);
 });
 
-
 // levels data
-$.getJSON('js/levels-data.json', data => {
-  let levelsHtml = '';
-  let dotsHtml = '';
+$.getJSON("js/levels-data.json", (data) => {
+  let levelsHtml = "";
+  let dotsHtml = "";
   data.forEach((level, i) => {
     levelsHtml += `
-      <div class="level-block ${i == 0 ? 'active' : ''}" data-index="${i}">
+      <div class="level-block ${i == 0 ? "active" : ""}" data-index="${i}">
         <div class="level-block__img">
           <img src="img/icons/${level.icon}">
           <div class="level-block__count">${level.count}</div>
@@ -25,41 +24,49 @@ $.getJSON('js/levels-data.json', data => {
     `;
     if (i % ITEMS_IN_SLIDE == 0) {
       dotsHtml += `
-        <div class="levels-slider__dot ${i == 0 ? 'active' : ''}" data-index="${parseInt(i / ITEMS_IN_SLIDE)}"></div>
+        <div class="levels-slider__dot ${
+          i == 0 ? "active" : ""
+        }" data-index="${parseInt(i / ITEMS_IN_SLIDE)}"></div>
       `;
     }
   });
-  $('.levels-slider__content').html(levelsHtml);
-  $('.levels-slider__dots').html(dotsHtml);
-  
-  let wSliderContent = $('.levels-slider__content').width();
+  $(".levels-slider__content").html(levelsHtml);
+  $(".levels-slider__dots").html(dotsHtml);
+
+  let wSliderContent = $(".levels-slider__content").width();
   if ($(window).outerWidth() > 767)
-    $('.levels-slider__content > *').css('min-width', wSliderContent / ITEMS_IN_SLIDE);
-  else 
-    $('.levels-slider__content > *').css('min-width', 80);
+    $(".levels-slider__content > *").css(
+      "min-width",
+      wSliderContent / ITEMS_IN_SLIDE
+    );
+  else $(".levels-slider__content > *").css("min-width", 80);
 
-  let wImg = $('.level-block__img').outerWidth();
-  $('.level-block__img').outerHeight(wImg);
+  let wImg = $(".level-block__img").outerWidth();
+  $(".level-block__img").outerHeight(wImg);
 
-  let hArr = $('.levels-slider__arrow').outerHeight();
-  $('.levels-slider__arrow').css('top', (wImg - hArr) / 2 + 5 + 'px');
+  let hArr = $(".levels-slider__arrow").outerHeight();
+  $(".levels-slider__arrow").css("top", (wImg - hArr) / 2 + 5 + "px");
 
-  let bLine = ($('.level-block__level').outerHeight() - $('.level-block__line').outerHeight()) / 2 + 5;
-  $('.level-block__line').css('bottom', bLine);
-  $('.level-block__line').last().remove();
+  let bLine =
+    ($(".level-block__level").outerHeight() -
+      $(".level-block__line").outerHeight()) /
+      2 +
+    5;
+  $(".level-block__line").css("bottom", bLine);
+  $(".level-block__line").last().remove();
 
   toLevel(0, data);
 });
 
-$('.btn-level-more-info').on('click', function() {
-  $('.loyality-program__info-block').slideToggle();
-  if ($(this).hasClass('hide-block')) {
-    $(this).text($(this).data('text'));
+$(".btn-level-more-info").on("click", function () {
+  $(".loyality-program__info-block").slideToggle();
+  if ($(this).hasClass("hide-block")) {
+    $(this).text($(this).data("text"));
   } else {
-    $(this).text('Hide info');
+    $(this).text("Hide info");
   }
-  $(this).toggleClass('hide-block');
-})
+  $(this).toggleClass("hide-block");
+});
 
 // height main img
 // let h = $('.loyality-program__info-block').outerHeight();
